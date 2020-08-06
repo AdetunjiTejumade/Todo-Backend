73a15b1039965509c95cffa36010fb03d6b0671a
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2020-08-06 22:36:22 +0100

    add test

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # mine
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 ]
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://localhost:8000'
+    'http://localhost:8000',
 )
 ROOT_URLCONF = 'todo_project.urls'
 
